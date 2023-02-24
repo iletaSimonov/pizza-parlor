ccc168780c5d5676a1c890e05c65ae2f44489719
Ileta Simonov <ileta.simonov@yahoo.com>
2023-02-23 18:42:20 -0800

    adds a cost to sizes and toppings

--- README.md
+++ README.md
@@ -4,9 +4,10 @@ Test: "It should return a pizza object with two properties for toppings and size
 Code: const myPizza = new Pizza(["anchovies", "pineapple"], "medium");
 Expected Output:  Pizza { toppings: ["anchovies", "pineapple"], size: "medium" }
 
-Test:
-Code:
-Expected Output:
+Test: "It should return the correct cost for a medium pizza with two toppings"
+Code: const pizza = new Pizza('Medium', ['Pepperoni', 'Mushrooms']);
+      const cost = pizza.getCost()
+Expected Output: cost.toEqual(11);
 
 Test:
 Code:
--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,30 @@ const pizza = {
   toppings: [],
   size: ''
 }
+
+function myPizza(toppings, size) {
+  this.size = size;
+  this.toppings = toppings;
+}
+
+pizza.prototype.getCost = function() {
+  let basePrice;
+
+  switch (this.size) {
+    case 'Small':
+      basePrice = 8;
+      break;
+    case 'Medium':
+      basePrice = 10;
+      break;
+    case 'Large':
+      basePrice = 12;
+      break;
+    default:
+      basePrice = 0;
+  }
+
+  const toppingPrice = this.toppings.length * 0.75;
+
+  return basePrice + toppingPrice;
+};
